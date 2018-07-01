4cee0809d182f42fbdd70c36c45984860deff5ed
jifeng.si <jifeng.si@qunar.com>
2018-07-01 13:46:17 +0800

    MOD:328. 奇偶链表

---
+++ src/java/list/ListNode.java
@@ -0,0 +1,12 @@
+package list;
+
+/**
+ * Created by wy on 2018/7/1.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/test/list/LinkedListTest.java
@@ -0,0 +1,34 @@
+package list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by wy on 2018/7/1.
+ */
+public class LinkedListTest {
+
+    private ListNode root = new ListNode(1);
+    @Before
+    public void init() {
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        root.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+    }
+    @Test
+    public void testOddEvenList() throws Exception {
+        OddEvenLinkedList oddEvenLinkedList = new OddEvenLinkedList();
+        ListNode head = oddEvenLinkedList.oddEvenList(root);
+        while(head != null){
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
