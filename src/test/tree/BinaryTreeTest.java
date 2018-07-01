90f9dc299a2d18265f4937d1b6567e658921712d
jifeng.si <jifeng.si@qunar.com>
2018-07-01 12:27:28 +0800

    MOD:94.二叉树的中序遍历

---
+++ src/test/tree/BinaryTreeTest.java
@@ -0,0 +1,32 @@
+package tree;
+
+import java.util.List;
+
+/**
+ * Created by wy on 2018/7/1.
+ */
+public class BinaryTreeTest {
+
+    private TreeNode root = new TreeNode(1);
+
+    @org.junit.Before
+    public void init() {
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(6);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.left = node5;
+    }
+
+    @org.junit.Test
+    public void testInorderTraversal() throws Exception {
+        BinaryTreeInorderTraversal inorderTraversal = new BinaryTreeInorderTraversal();
+        List<Integer> result = inorderTraversal.inorderTraversal(root);
+        System.out.println(result);
+    }
+}
